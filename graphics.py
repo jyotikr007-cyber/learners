bf5e0973e32f50d3d3e58f3b485a7fffec538500
jyoti <jyotikumarimgr04@gmail.com>
2026-04-09 18:21:31 +0530

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,145 @@
+# #draw a square for making graphics write
+# import turtle
+# #make an object
+# t=turtle.Turtle()
+# t.speed(1)
+
+# t.hideturtle
+# t.begin_fill("red")# assign a value for object
+# for i in range(4):
+#     #here i'm making a square so i'm writing 4
+#     t.forward(100)# write the distance
+#     t.left(90)# write angle here
+#     t.end_fill()
+# import turtle
+# t=turtle.Turtle()
+# sn=turtle.Screen()
+# sn.bgcolor("white")
+# #draw a line
+# import turtle
+# t=turtle.Turtle()
+# t.speed(1)
+# t.forward(500)# the distance
+# #draw a circle
+# import turtle
+# t=turtle.Turtle()
+# t.circle(100)# the radius of the circle
+# import turtle
+# j=turtle.Turtle()
+# j.speed(1)
+# j.begin_fill()
+# j.fillcolor("violet")
+# j.circle(50)
+# j.end_fill()
+
+# import turtle
+# j=turtle.Turtle()
+# j.speed(1)
+# j.up()
+# j.goto(70,20)
+# j.hideturtle()
+# j.begin_fill()
+# j.fillcolor("brown")
+# j.circle(80)
+# j.end_fill()
+# j.hideturtle()
+
+
+
+
+
+
+# #draw a circle & fill colour
+# import turtle
+# j=turtle.Turtle()
+# j.speed(1)#to control the speed
+# j.begin_fill()
+# j.fillcolor("red")
+
+# # for filling the color
+# # and it should be before making the graphics
+# j.up()
+# j.goto(45,32)
+# j.down()
+# j.reset()
+# j.circle(60)
+
+# #and at the end close it 
+# j.end_fill()
+
+
+
+
+# #make a rectangle and insert 3 circles in this
+# import turtle
+# t=turtle.Turtle()
+# t.speed(1)
+# t.pensize(3)#increase the size of pen(thickness)
+# #for making the rectangle
+# t.forward(250)#draw the line 
+# t.left(90)#turns the angle of turtle
+# t.forward(300)
+# t.left(90)
+# t.fd(250)
+# t.left(90)
+# t.fd(300)#fd stands for "forward"
+# t.left(90)
+# t.fd(150)
+# t.hideturtle()#for hiding the turtle
+# #making circle in this
+# t.up()#don't draw the line
+# t.goto(70,110)#x,y coordinates
+# t.down()#draws the line
+# t.circle(20)
+# t.up()
+# t.goto(70,220)
+# t.down()
+# t.circle(20)
+# t.up()
+# t.goto(180,220)
+# t.down()
+# t.circle(20)
+# t.up()
+# t.goto(180,110)
+# t.down()
+# t.circle(20)
+
+# import turtle
+# t=turtle.Turtle()
+# t.speed(1)
+# t.left(90)
+# t.forward(200)
+# t.left(90)
+# t.forward(100)
+# t.leftt(90)
+# t.forward(50)
+# t.left(90)
+# t.forward(200)
+# t.left(90)
+# t.forward(50)
+# t.left(90)
+# t.forward(200)
+
+import turtle
+t=turtle.Turtle()
+t.up()
+t.got(0,-300)
+t.down()
+t.goto(0,400)
+def rect(color):
+    t.begin_fill()
+    t.color("orange")
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(400)
+    t.right(90)
+    t.end_fill()
+    t.forward(100)
+    t.right(90)
+    
+
+
+
+
