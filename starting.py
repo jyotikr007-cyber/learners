e188b448410741e66c6b3660e68c24a9b7747724
jyoti <jyotikumarimgr04@gmail.com>
2026-04-09 18:32:37 +0530

    Add files via upload

---
+++ starting.py
@@ -0,0 +1,214 @@
+
+a=1
+while a<=5:
+    print(a*a)
+    a+=1
+
+def age(num):
+    if age==18:
+      return
+    print("eligible")
+    
+    print("not eligible")
+age(18)    
+
+#using function check even or odd
+def number(o):
+   while (number==8):
+      if number%2==0:
+         
+        return
+
+print("yes!") 
+ 
+print("no")
+number(4)
+
+# def num(y):
+#    if num%2==0:
+#     print("even number")
+    
+#     print("odd number")
+      
+# num(78)
+def add_number(*name):
+   return sum(name)
+result=add_number(14,8,7)
+print(result)
+pass
+print("dying")
+
+# def diff_num(*di):
+#    return 
+# re=diff_num(100,5)
+# print(re)
+def multiply(*number):
+   total=1
+   for num in number:
+      total*=num
+      return total
+   print(multiply(4,7))
+
+pass
+print("die")
+
+def diff(*di):
+  for i in range(120):
+     return sum(di)
+
+re=diff(100,5,10)
+print(re)
+
+
+
+def greet(*jy):
+   for jy in jy:
+      print(f"Hello{jy}")
+
+greet("kumari","munger","life")  
+
+
+
+
+# def  greeting(*y):
+#     for y in y:
+     
+#         print(f"good{y}  bad{y}")
+
+# greeting("morning","evening")
+
+
+
+# t=input("enter your name")
+# u=input("enter your age")
+
+# print(f"your{t},name{u}")
+
+# def details(**kwargs):
+#    for key,value in kwargs:
+#       print(f"{key}:{value}")
+
+
+# details(name='jyoti',age=25)
+
+#recurssion
+
+# def show(n):
+#    if n<=20:
+#       print(n)
+#       show(n-1)
+# show(5)  
+
+
+
+def no(n):
+   if n==0:
+      return
+   print(n)
+   no(n-1)
+no(5)   
+
+
+# def even(m,j):
+#    if m%2!=0:
+#       if j%2!=0:
+#          return m 
+#       return j
+#       #recursion works as break condition in loops
+#    print(m)
+#    print(j)
+#    even(m-1,j)
+#    even(j-1)
+# even(80,4)
+
+def eve(m,j):
+   if m%2!=0 or j%2!=0:
+      return
+   print(f"both odd:{m},{j}")
+   return m
+   return(m-1,j-1)
+eve(80,14)
+
+
+# def od(ch):
+#    while ch==10:
+#       if ch==2:
+#          return
+#       print(ch)
+# od("you")
+
+def square(a,b):
+   return sum
+print(sum)
+square(5,4)
+square(4,3)
+
+# f=open("filio.txt","a")
+# dov=f.write("you are also genious body")
+# print(dov)
+
+f=open("filio.txt","r")
+dove=f.read()
+print(dove)
+
+
+f=open("kite.txt","w")
+hope=f.write("cool but not fool! literaly feels crazy")
+print(hope)
+
+f=open("filio.txt","r")
+hopeless=f.readline(7)
+print(hopeless)
+
+
+def name(jyoti):
+   print(jyoti)
+name("you")
+
+
+def name(**kwjy):
+   return kwjy
+naam=name()
+print(naam)
+
+#arbitrary arguments
+def details(**kwdet):
+   for key,value in kwdet.items():
+      print(f"{key}:{value}")
+details(name="mohan" ,age=15)
+
+
+def std(**kwstudent):
+   for key,value in kwstudent.items():
+      print(f"{key}:{value}")
+std(reg=25151144010,roll=251511044)  
+
+
+# def frnds(**kwfriend):
+#    while True:
+#          print(kwfriend)
+#       # return 471253698
+# fr=frnds(32)
+# print()     
+
+x=4
+while x<=11:
+   x=x+6
+   print(x)
+
+pass 
+print("edddddd........🤪")
+
+y=4
+while y<=11:
+   print(y)
+   y+=6
+
+def lt(man):
+   y=4
+   while y<=11:
+      return man
+   y=+1
+lit=lt(14)
+print(lit)   
+   
